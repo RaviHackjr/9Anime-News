40ded745e6207d5fba056aa4c10c8c41ad27c5c8
Ravi Gurjar <99577613+RaviHackjr@users.noreply.github.com>
2025-02-18 15:47:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 API_ID = "29478593" #API ID get it from telegram.org
 API_HASH = "24c3a9ded4ac74bab73cbe6dafbc8b3e" #API HASH get it from telegram.org
-BOT_TOKEN = "7426089831:AAFCCHq9EBxyt2LCj4irZ6As-UyGUnHN7zg" #Bot Token get it from @BotFather
+BOT_TOKEN = "7110129557:AAGb99GCsE1rgnvstiq1iyCmqTr8r0jeXUE" #Bot Token get it from @BotFather
 URL_A = "https://myanimelist.net/rss/news.xml" #News RSS Feed Url 2
 URL_B = "https://cr-news-api-service.prd.crunchyrollsvc.com/v1/en-US/rss" #News RSS Feed Url 2
 START_PIC = "https://images5.alphacoders.com/113/thumb-1920-1134698.jpg" #Startup Pic
